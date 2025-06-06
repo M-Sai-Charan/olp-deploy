57fe6a4afd5a6f0918aae082c4a01e1071e47f09
UpendraV1 <msunnylive@gmail.com>
2025-06-06 19:35:39 +0530

    olp form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'olp';
+  @HostListener('document:contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (
+      event.key === 'F12' ||
+      (event.ctrlKey && event.shiftKey && ['i', 'j', 'c'].includes(event.key.toLowerCase())) ||
+      (event.ctrlKey && event.key.toLowerCase() === 'u')
+    ) {
+      event.preventDefault();
+    }
+  }
 }
