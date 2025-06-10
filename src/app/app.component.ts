c7698168af34f96a31d584109671493b58748d50
UpendraV1 <msunnylive@gmail.com>
2025-06-10 06:39:57 +0530

    olp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,19 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'olp';
-  @HostListener('document:contextmenu', ['$event'])
-  onRightClick(event: MouseEvent) {
-    event.preventDefault();
-  }
+  // @HostListener('document:contextmenu', ['$event'])
+  // onRightClick(event: MouseEvent) {
+  //   event.preventDefault();
+  // }
 
-  @HostListener('document:keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
-    if (
-      event.key === 'F12' ||
-      (event.ctrlKey && event.shiftKey && ['i', 'j', 'c'].includes(event.key.toLowerCase())) ||
-      (event.ctrlKey && event.key.toLowerCase() === 'u')
-    ) {
-      event.preventDefault();
-    }
-  }
+  // @HostListener('document:keydown', ['$event'])
+  // onKeyDown(event: KeyboardEvent) {
+  //   if (
+  //     event.key === 'F12' ||
+  //     (event.ctrlKey && event.shiftKey && ['i', 'j', 'c'].includes(event.key.toLowerCase())) ||
+  //     (event.ctrlKey && event.key.toLowerCase() === 'u')
+  //   ) {
+  //     event.preventDefault();
+  //   }
+  // }
 }
